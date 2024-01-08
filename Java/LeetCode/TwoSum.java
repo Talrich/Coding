d192433be2e560baf753e3a1b337966fa356b53f
Talrich <149553968+Talrich@users.noreply.github.com>
2024-01-08 13:53:34 -0500

    Add files via upload

--- Java/LeetCode/TwoSum.java
+++ Java/LeetCode/TwoSum.java
@@ -1,20 +1,27 @@
-public class TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int pos1 = 0; 
+        int pos2 = pos1 + 1;
+        int[] indices = new int[2];
         int i = 0;
-        int pos1 = nums[i];
-        int pos2 = nums[i+1];
         while (i < nums.length)
         {
-            if ((pos1 + pos2) == target)
+            if (nums[pos1] + nums[pos2] == target)
             {
-                
+                indices[0] = pos1; 
+                indices[1] = pos2; 
+                return indices;
+            }
+            else if (pos2 < nums.length-1)
+            {
+                pos2++;
+            }
+            else
+            {
+                pos1++;
+                pos2 = pos1+1;
             }
         }
-    }
-
-    public static void main(String[] args)
-    {
-
+        return indices;
     }
 }
